1d6a3afaab6574683b8a80ff5abcb090068eb5e4
max <pingwinno@gmail.com>
2024-12-10 16:06:28 +0200

    initial commit

---
+++ src/main/java/com/example/speaker_web_gate/SpeakerWebGateApplication.java
@@ -0,0 +1,13 @@
+package com.example.speaker_web_gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakerWebGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakerWebGateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/speaker_web_gate/SpeakerWebGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speaker_web_gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakerWebGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
